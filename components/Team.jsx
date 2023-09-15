f4b232b6378614f119d5651387625eae84d38f24
garunAMT <21ME02022@iitbbs.ac.in>
2023-09-16 01:44:11 +0530

    changed scholars section

--- components/Team.jsx
+++ components/Team.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Image from "next/image";
 import { StudentNames } from "@/constants";
 
@@ -7,7 +7,7 @@ const Team = () => {
     <section>
       {/* <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
         <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold">
+          <h2 className="mb-4 text-4xl font-extrabold">
             Research Scholars
           </h2>
         </div>
@@ -41,30 +41,30 @@ const Team = () => {
         </div>
       </div> */}
 
-      <section class="text-gray-400body-font">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="flex flex-col text-center w-full mb-20">
-            <h1 class="text-2xl font-medium title-font mb-4 ">
+      {/* <section className="text-gray-400body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h1 className="text-2xl font-medium title-font mb-4 ">
               REASEARCH SCHOLARS
             </h1>
       
           </div>
 
-          <div class="flex flex-wrap -m-4">
+          <div className="flex flex-wrap -m-4">
             {StudentNames.map((student, index) => {
               return (
-                <div class="p-4 lg:w-1/4 md:w-1/2" key={student.id}>
-                  <div class="h-full flex flex-col items-center text-center">
+                <div className="p-4 lg:w-1/4 md:w-1/2" key={student.id}>
+                  <div className="h-full flex flex-col items-center text-center">
                     <Image
                       alt="team"
-                      class="flex-shrink-0 rounded-lg w-full  object-cover object-center mb-4"
+                      className="flex-shrink-0 rounded-lg w-full  object-cover object-center mb-4"
                       src={student.image}
                     />
-                    <div class="w-full">
-                      <h2 class="title-font font-medium text-lg">
+                    <div className="w-full">
+                      <h2 className="title-font font-medium text-lg">
                         {student.name}
                       </h2>
-                      <h3 class="text-gray-500 mb-3">{student.degree}</h3>
+                      <h3 className="text-gray-500 mb-3">{student.degree}</h3>
                     </div>
                   </div>
                 </div>
@@ -72,7 +72,34 @@ const Team = () => {
             })}
           </div>
         </div>
-      </section>
+      </section> */}
+      <h1 className="mb-4 pt-12 text-4xl font-extrabold text-center">Research Scholars</h1>
+      {StudentNames.map((student, index) => {
+        return (
+          <div className="body-font border-2 border-black lg:mx-24 mx-2 my-4" key={student.id}>
+            <div className="container mx-auto flex px-2 py-2 md:flex-row flex-col items-center">
+              <div className="lg:max-w-lg lg:w-[150px] md:w-1/2 w-5/6 md:mb-0 mb-10">
+                <Image
+                  className="object-cover object-center rounded border-2"
+                  alt="hero"
+                  src={student.image}
+                />
+              </div>
+              <div className="lg:flex-grow md:w-1/2 lg:pl-8 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                <h1 className="title-font sm:text-4xl text-3xl font-medium">
+                  {student.name}
+                </h1>
+                <p className="leading-relaxed text-xl mb-2">
+                  {student.degree}
+                </p>
+                <p className="mb-4 leading-relaxed">
+                  {student.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </section>
   );
 };
