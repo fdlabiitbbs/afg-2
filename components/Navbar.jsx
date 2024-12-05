cb7f1a19f58814519699901e13f0de97a9e49435
garunAMT <21ME02022@iitbbs.ac.in>
2024-12-05 11:28:14 +0530

    modified google scholar button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import logo from "@/assets/afg_gif.gif";
+import googleScholar from "@/assets/googleScholar.jpg"
 
 function Navtwo() {
   const [navbar, setNavbar] = useState(false);
@@ -215,7 +216,7 @@ function Navtwo() {
                     className="py-2 px-6 cursor-pointer text-xl text-white lg:hover:bg-slate-600 rounded-md flex items-center"
                     href="https://scholar.google.co.in/citations?user=1N1daS8AAAAJ&hl=en"
                   >
-                    <svg
+                    {/* <svg
                       className="text-xl bi bi-google w-6 h-6"
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
@@ -227,7 +228,8 @@ function Navtwo() {
                         d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"
                         fill="white"
                       ></path>
-                    </svg>
+                    </svg> */}
+                    <Image src={googleScholar} className="h-full w-24" />
                   </Link>
                 </li>
 
